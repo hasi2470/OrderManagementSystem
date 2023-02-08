6cc11aafa30106f7b022d1203c8e518e4a6d8b69
Hasitha Gajula <g.hasi2470@gmail.com>
2023-02-08 19:41:48 +0530

    Scaffold the DbContext and models from EF Core db first

---
+++ OrderManagementSystemApi/OrderManagementSystemApi/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderManagementSystemApi.Models;
+
+[Table("Customer")]
+[Index("UserName", Name = "UQ__Customer__C9F284564B02FA38", IsUnique = true)]
+public partial class Customer
+{
+    [Key]
+    [Column("Cust_Id")]
+    public int CustId { get; set; }
+
+    [StringLength(50)]
+    public string Name { get; set; } = null!;
+
+    [StringLength(100)]
+    public string? Adderess { get; set; }
+
+    [Column("Contact_num")]
+    [StringLength(13)]
+    public string? ContactNum { get; set; }
+
+    [StringLength(20)]
+    public string UserName { get; set; } = null!;
+
+    [StringLength(20)]
+    public string Password { get; set; } = null!;
+}
