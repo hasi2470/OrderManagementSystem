bb84f878626ed701def2ba54133e38fceb793e90
Hasitha Gajula <g.hasi2470@gmail.com>
2023-02-08 19:02:15 +0530

    Created Customer Table

---
+++ OrderManagementSystemDb/OrderManagementSystemDb/Tables/Customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Cust_Id] INT NOT NULL PRIMARY KEY,
+	[Name] nvarchar(50) NOT NULL,
+	[Adderess] nvarchar(100),
+	[Contact_num] nvarchar(13),
+	[UserName] nvarchar(20) NOT NULL UNIQUE,
+	[Password] nvarchar(20) NOT NULL
+)
